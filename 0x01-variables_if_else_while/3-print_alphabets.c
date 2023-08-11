ea27c9879821a6747ccc9f36d676d00ebfa34a18
root <root@anoly23.anoly23.ng.com>
2023-08-11 18:33:18 +0100

    Prints letters of the alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main -entry point
+ * Description - program starts here
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	int c;
+
+	for (c = 97; c < 123; c++)
+	{
+		char ch = (char)c;
+
+		printf("%c", ch);
+	}
+	for (c = 65; c < 91; c++)
+	{
+		char ch = (char)c;
+
+		printf("%c", ch);
+	}
+
+	printf("\n");
+	return (0);
+
+}
