924f74c17935306e51544aadfa32fd07d83221db
root <root@anoly23.anoly23.ng.com>
2023-08-11 19:36:46 +0100

    Prints the hexadecimal numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description - program starts here
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	int cn;
+
+	int new = 10;
+	char newline = (char)new;
+
+	for (cn = 0; cn < 10; cn++)
+	{
+		putchar(cn);
+	}
+	for (cn = 97; cn < 103; cn++)
+	{
+		putchar(cn);
+	}
+
+	putchar(newline);
+	return (0);
+}
