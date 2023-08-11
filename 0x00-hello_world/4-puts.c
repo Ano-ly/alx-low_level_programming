e5721e308c4d10485ac834e9953614fcf2834812
root <root@anoly23.anoly23.ng.com>
2023-08-11 04:26:44 +0100

    Prints a long sentence using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description - the program starts here
+ * Return: always 0
+*/
+
+int main(void)
+{
+	char sent[60] = "Programming is like building a multilingual puzzle";
+
+	puts(sent);
+	return (0);
+}
