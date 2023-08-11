142c271d513e43366292a117c066f74054492dee
root <root@anoly23.anoly23.ng.com>
2023-08-11 05:36:01 +0100

    Tailors output to required format

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,18 +10,19 @@ int main(void)
 {
 	long int li = 3;
 	long long int lli = 4;
+	char a = 65;
 
-	int csize = sizeof("a");
+	int csize = sizeof(a);
 	int isize = sizeof(3);
 	int lisize = sizeof(li);
 	int llisize = sizeof(lli);
 	int fsize = sizeof(3.33f);
 
-	printf("Size of a char: %d byte(s)", csize);
-	printf("Size of an int: %d byte(s)", isize);
-	printf("Size of a long int: %d byte(s)", lisize);
-	printf("Size of a long long int: %d byte(s)", llisize);
-	printf("Size of a float: %d byte(s)", fsize);
+	printf("Size of a char: %d byte(s)\n", csize);
+	printf("Size of an int: %d byte(s)\n", isize);
+	printf("Size of a long int: %d byte(s)\n", lisize);
+	printf("Size of a long long int: %d byte(s)\n", llisize);
+	printf("Size of a float: %d byte(s)\n", fsize);
 
 	return (0);
 }
