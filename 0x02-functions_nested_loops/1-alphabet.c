644c0e7d095ca4abb17d161f6db6fe98c497097d
root <root@anoly23.anoly23.ng.com>
2023-08-15 17:05:07 +0100

    Prints the letters of the alphabet, and involves the use of functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the letters of the alphabet
+ *
+ * Returns: void
+*/
+
+void print_alphabet(void)
+{
+	int i = 97;
+
+	while (i < 123)
+	{
+		_putchar (i);
+		i++;
+	}
+}
+
