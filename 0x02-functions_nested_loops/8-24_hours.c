1d36546a20ccac61aef510c1e842704b69f465ad
root <root@anoly23.anoly23.ng.com>
2023-08-17 00:55:31 +0100

    Prints time between 00:00 and 23:59

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,12 +16,28 @@ void jack_bauer(void)
 
 	for (i = 48; i < 51; i++)
 	{
-	for (j = 48; j < 52; j++)
+	if ((i == 50) && (j == 52))
 	{
+		break;
+	}
+	for (j = 48; j < 58; j++)
+	{
+	if ((i == 50) && (j == 52))
+	{
+		break;
+	}
 	for (k = 48; k < 54; k++)
 	{
+	if ((i == 50) && (j == 52))
+	{
+		break;
+	}
 	for (l = 48; l < 58; l++)
 	{
+		if ((i == 50) && (j == 52) && (k == 48) && (l == 48))
+		{
+			break;
+		}
 		_putchar(i);
 		_putchar(j);
 		_putchar(58);
