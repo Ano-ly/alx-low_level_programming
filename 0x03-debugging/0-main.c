bbdfa8404f003bd207cc5e58e1d42dda85a4727a
root <root@anoly23.anoly23.ng.com>
2023-08-17 17:26:36 +0100

    Corrects code to suit project requirements

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,10 +1,8 @@
 #include "main.h"
 
 /**
- * main - test if positive_or_negative(int i) function prints right output wh
- * en i = 0
- * Description - tests a function
- * Return: 0
+* main - tests function that prints if integer is positive or negative
+* Return: 0
 */
 
 int main(void)
