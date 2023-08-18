683c144b5110bf6f3bf02c3c752e6e18f8cf6553
root <root@anoly23.anoly23.ng.com>
2023-08-18 05:30:30 +0100

    Corrects code of 8-print_squares

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -16,14 +16,11 @@ void print_square(int size)
 
 		for (i = 0; i < size; i++)
 		{
-			j = 0;
-			while (j < size)
+			for (j = 0; j < size; j++)
 			{
 				_putchar(35);
-				j++;
 			}
 			_putchar(10);
-			i++;
 
 		}
 	}
