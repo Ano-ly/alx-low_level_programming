3c7d92109409f1707919558e76d00f5fbda53eb0
root <root@anoly23.anoly23.ng.com>
2023-08-21 18:58:23 +0100

    Starts with new project

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+  * print_rev - prints the reversed form of the string argument
+  * @s: string whose reversed form is to be printed
+  * Description - takes a string argument and prints its reversed form
+  * Return: void
+*/
+
+void print_rev(char *s)
+{
+	int i;
+	int _length;
+	int value;
+
+	_length = strlen(s);
+
+	for (i = _length - 1; i > -1; i--)
+	{
+		value = s[i];
+		printf("%c", value);
+		printf("\n")
+	}
+
+}
