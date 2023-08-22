3354a81dc6f747fb51be11c9894dd9fada06f882
root <root@anoly23.anoly23.ng.com>
2023-08-22 04:43:14 +0100

    Adds file 100-atoi.c to repository

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -12,6 +12,11 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+int get_int(char c);
+int get_start(char *str);
+int get_end(char *str, int start);
+int get_sign(char *str, int start);
 
 
 #endif
---
+++ 0x05-pointers_arrays_strings/testing2.c
@@ -0,0 +1,8 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+	printf("%d", get_start("---++++ -++ Sui - te -   402 #cisfun :)"));
+	return (0);
+}
