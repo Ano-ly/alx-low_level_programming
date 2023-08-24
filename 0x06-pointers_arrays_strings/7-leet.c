f2d37bcca2bad5a4b125ce26356652491afa10b4
root <root@anoly23.anoly23.ng.com>
2023-08-24 12:01:23 +0100

    Corrects 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,36 +1,5 @@
 #include "main.h"
-
-/**
- * _strlen - computes the length of a string
- * @str: string whose length is to be computed
- * Description - computes length of string
- * Return: integer, length of string
-*/
-
-int _strlen(char *str)
-{
-	int i;
-	int lenn;
-
-	i = 0;
-	lenn = 0;
-
-	while (str[i] != 0)
-	{
-		lenn++;
-		i++;
-	}
-	return (lenn);
-}
-
-/**
- * leet - converts string to leet
- * @s: string to be converted
- *
- * Description - letters a,e,o,t,l,A,E,O,T,L are converted to
- * 4,3,0,7,1,4,3,0,7,1 respectively
- * Return: pointer to converted string
-*/
+#include <string.h>
 
 char *leet(char *s)
 {
@@ -41,7 +10,7 @@ char *leet(char *s)
 	int j;
 	int l;
 
-	l = _strlen(s);
+	l = strlen(s);
 	i = 0;
 
 	for (i = 0; i < l; i++)
