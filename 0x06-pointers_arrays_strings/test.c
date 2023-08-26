f96c7d00e71fe580e512fccf40d2a5d7c5670623
root <root@anoly23.anoly23.ng.com>
2023-08-26 05:44:55 +0100

    Adds advanced task 104-print_buffer.c

--- 0x06-pointers_arrays_strings/test.c
+++ 0x06-pointers_arrays_strings/test.c
@@ -1,32 +1,17 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	int count;
+    char buffer[] = "This is a string!\0And this is the rest of the #buffer :)\1\2\3\4\5\6\7#cisfun\n\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x20\x21\x34\x56#pointersarefun #infernumisfun\n";
 
-	char me[] = "amarachi\0";
-	count = 0;
-
-	if (me[8] || me[8] == 0)
-	{
-		printf("impossibe");
-	}
-	else
-	{
-		printf("never");
-	}
-	while (1 == 1)
-	{
-		if (me[count] == 0 && me[count + 1] == 0)
-		{
-			printf("%d", count);
-		}
-		else if (!(me[count] != 0 || me[count + 1]))
-			break;
-		count++;
-	}
-	printf("%d", count);
-
-	return (0);
+    printf("%s\n", buffer);
+    printf("---------------------------------\n");
+    print_buffer(buffer, sizeof(buffer));
+    return (0);
 }
