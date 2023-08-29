320e53e8c04f47010c75f55bf36d4cb0ce4b1387
root <root@anoly23.anoly23.ng.com>
2023-08-29 15:15:58 +0100

    Completes code for chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,14 +1,26 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_chessboard - prints chessboard
- * @a: character to be printed
- * Description - prints chessboard
+ * print_chessboard - prints a chessboard
+ * @a: string to be printed
+ * Definition - prints chessboard
  * Return: void
 */
 
 void print_chessboard(char (*a)[8])
 {
-	printf("%p", a);
+	int i;
+	int j;
+	int pawn;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			pawn = a[i][j];
+			_putchar(pawn);
+		}
+		_putchar(10);
+	}
+
 }
