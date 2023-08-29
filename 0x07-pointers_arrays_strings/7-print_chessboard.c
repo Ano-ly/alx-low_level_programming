c2fa7ff8d461007ab1893587e03a713a38881aee
root <root@anoly23.anoly23.ng.com>
2023-08-29 05:52:15 +0100

    Adds file 7-print_chessboard.c to repo

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - prints chessboard
+ * @a: character to be printed
+ * Description - prints chessboard
+ * Return: void
+*/
+
+void print_chessboard(char (*a)[8])
+{
+	printf("%p", a);
+}
