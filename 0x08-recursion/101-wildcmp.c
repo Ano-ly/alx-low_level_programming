e2041b87027c0357b33cccf4b673470029b24c32
root <root@anoly23.anoly23.ng.com>
2023-08-31 04:08:34 +0100

    Makes file 101 suit Betty's requirements

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -3,10 +3,28 @@
 int comp(char *s1, char *s2, int n1, int n2);
 int end(char *s, int n);
 
+/**
+ * wildcmp - compares two strings
+ * @s1: first string to be compared
+ * @s2: second string to be compared
+ * Description - compares two strings
+ * Return: 0 if not identical, 1 if identical
+*/
+
 int wildcmp(char *s1, char *s2)
 {
 	return (comp(s1, s2, 0, 0));
 }
+
+/**
+ * comp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ * @n1: recursion variable for indexing s1
+ * @n2: recursion variable for indexing s2
+ * Description - compares two strings using recursion
+ * Return: 0 if not identical, 1 if identical
+*/
 
 int comp(char *s1, char *s2, int n1, int n2)
 {
@@ -22,7 +40,7 @@ int comp(char *s1, char *s2, int n1, int n2)
 	{
 		return (comp(s1, s2, n1, end(s2, n2)));
 	}
-	if ((s2[n2] == '*') || (s2[n2 -1] == '*'))
+	if ((s2[n2] == '*') || (s2[n2 - 1] == '*'))
 	{
 		return (comp(s1, s2, n1 + 1, end(s2, n2)));
 	}
@@ -31,6 +49,14 @@ int comp(char *s1, char *s2, int n1, int n2)
 		return (0);
 	}
 }
+
+/**
+ * end - computes the index of the next character that is not '*'
+ * @s: string
+ * @n: starting index
+ * Description - is called by comp function
+ * Return: index of next character that is not '*'
+*/
 
 int end(char *s, int n)
 {
