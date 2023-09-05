7220b6be3c9fdd12ce9daaf3f2e5465486f8f45a
root <root@anoly23.anoly23.ng.com>
2023-09-05 22:44:08 +0100

    Makes slight changes to file 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 
+int calc_len(char *s1, char *s2);
+
 /**
  * str_concat - concatenates strings
  * @s1: first string
@@ -16,40 +18,78 @@ char *str_concat(char *s1, char *s2)
 	int i;
 	int j;
 
-	len = 0;
+	len = calc_len(s1, s2);
 	i = 0;
 	j = 0;
 
-	if (s1 == NULL && s2 == NULL)
-		return (NULL);
-	while (s1[i] != '\0' && s1 != NULL)
-	{
-		len++;
-		i++;
-	}
-	i = 0;
-	while (s2[i] != '\0' && s2 != NULL)
-	{
-		len++;
-		i++;
-	}
 	if (len == 0)
 		return (NULL);
 	p = malloc(sizeof(char) * (len + 1));
+
 	if (p == NULL)
 		return (NULL);
 	i = 0;
-	while (s1[i] != '\0' && s1 != NULL)
+
+	if (s1 != NULL)
 	{
-		p[i] = s1[i];
-		i++;
+		while (s1[i] != '\0')
+		{
+			p[i] = s1[i];
+			i++;
+		}
 	}
-	while (s2[j] != '\0' && s2 != NULL)
+	if (s2 != NULL)
 	{
-		p[i] = s2[j];
-		i++;
-		j++;
+		while (s2[j] != '\0')
+		{
+			p[i] = s2[j];
+			i++;
+			j++;
+		}
 	}
 	p[i] = '\0';
+
 	return (p);
+}
+
+/**
+* calc_len - calculates the required length of memory space
+* @s1: first string to be considered
+* @s2: second string to be considered
+* Definition - calculates len
+* Return: value of len
+*/
+
+int calc_len(char *s1, char *s2)
+{
+	int len;
+	int i;
+
+	len = 0;
+	i = 0;
+
+	if (s1 != NULL)
+	{
+		while (s1[i] != '\0')
+		{
+			len++;
+			i++;
+		}
+	}
+	i = 0;
+
+	if (s2 != NULL)
+	{
+		while (s2[i] != '\0')
+		{
+			len++;
+			i++;
+		}
+	}
+
+	if (len == 0)
+	{
+		return (0);
+	}
+	return (len);
 }
