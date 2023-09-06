e346e4fd7244dfefe164634a25deb78ad156d53d
root <root@anoly23.anoly23.ng.com>
2023-09-06 02:07:23 +0100

    makes changes to file 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -32,7 +32,7 @@ int **alloc_grid(int width, int height)
 		p[i] = malloc(sizeof(int) * width);
 		if (p[i] == NULL)
 		{
-			for (i = i - 1; i >= 0; i--)
+			for (; i >= 0; i--)
 				free(p[i]);
 			return (NULL);
 		}
