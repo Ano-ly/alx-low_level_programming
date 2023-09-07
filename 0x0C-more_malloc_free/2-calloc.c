d462320350f77a266b3e29731826092559c8fa50
root <root@anoly23.anoly23.ng.com>
2023-09-07 21:15:53 +0100

    Adds file 2-calloc.c to repo

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+include <stdlib.h>
+
+/**
+ * _calloc - a function that allocates memory for an array
+ * @nmemb: number of elements
+ * @size: size of type to be added to array
+ * Definition - allocates memory for an array
+ * Return: pointer to allocated memory space; NULL if function fails
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	/*unsigned int i;*/
+	void *pp;
+
+	if (nmemb <= 0 || size <= 0)
+	{
+		return (NULL);
+	}
+	pp = malloc(nmemb * size);
+	if (pp == NULL)
+	{
+		return (NULL);
+	}
+	/*for (i = 0; i < nmemb; i++)*/
+	/*{*/
+	/*	pp[i] = 0;*/
+	/*}*/
+	return (pp);
+}
