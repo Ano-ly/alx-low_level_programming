799b281db1e93532f6f19761e904954c36b44c67
root <root@anoly23.anoly23.ng.com>
2023-09-11 20:05:33 +0100

    Adds file dog.h to repo

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This project is all about structs and typedef in C
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+
+/**
+* struct dog - a new struct
+* @name: name of dog
+* @age: age of dog, float
+* @owner: name of owner of dog
+*
+* Description - characteristics for a new dog
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
