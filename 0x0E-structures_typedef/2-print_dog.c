50c819c98f52f638877022e9a9d0ff811b1657ca
root <root@anoly23.anoly23.ng.com>
2023-09-12 01:45:32 +0100

    Makes slight changes to file 5

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -21,17 +21,14 @@ void print_dog(struct dog *d)
 
 	if (d != NULL)
 	{
-	if (n == NULL)
-		printf("Name: (nil)\n");
-	else
-		printf("Name: %s\n", d->name);
-	if (a)
-		printf("Age: (nil)\n");
-	else
+		if (n == NULL)
+			printf("Name: (nil)\n");
+		else
+			printf("Name: %s\n", d->name);
 		printf("Age: %f\n", d->age);
-	if (o == NULL)
-		printf("Owner: (nil)\n");
-	else
-		printf("Owner: %s\n", d->owner);
+		if (o == NULL)
+			printf("Owner: (nil)\n");
+		else
+			printf("Owner: %s\n", d->owner);
 	}
 }
