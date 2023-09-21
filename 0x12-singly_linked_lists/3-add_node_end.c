f7be4d5c8505e738e5ffbc4906b8fcddcac428eb
root <root@anoly23.anoly23.ng.com>
2023-09-21 20:24:34 +0100

    Adds file 3-add_node_end.c

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+
+/**
+ * add_node_end - adds node to the end of a linked list
+ * @head: pointer to pointer that points to first element of linked list
+ * @str: string value for node in linked list
+ * Definition - adds new node or elemet to end of linked list
+ * by traversing the list until it reaches a node that points to null
+ * Return: pointer to the new node
+*/
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *iter;
+	list_t *new;
+	char *str_node;
+
+	str_node = strdup(str);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = str_node;
+	new->len = _strlen(str_node);
+	new->next = NULL;
+
+	iter = *head;
+	if (iter == NULL)
+		*head = new;
+	else
+	{
+		while (iter != NULL)
+		{
+			if (iter->next == NULL)
+			{
+				iter->next = new;
+				break;
+			}
+			iter = iter->next;
+		}
+	}
+	return (new);
+
+}
+
+
