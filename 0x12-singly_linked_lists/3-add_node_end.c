638e4a0cba88c7b937aba0792b54561ebd4f7608
root <root@anoly23.anoly23.ng.com>
2023-09-21 20:40:38 +0100

    Adds _strlen function to file 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -47,4 +47,25 @@ list_t *add_node_end(list_t **head, const char *str)
 
 }
 
+/**
+  * _strlen - computes the length of a string
+  * @str: string whose length is to be computed
+  * Description - computes length of string
+  * Return: integer, length of string
+  */
 
+int _strlen(const char *str)
+{
+	int i;
+	int lenn;
+
+	i = 0;
+	lenn = 0;
+
+	while (str[i] != 0)
+	{
+		lenn++;
+		i++;
+	}
+	return (lenn);
+}
