fa6bf12006b643db4b88dc8b97be37392bd321c3
root <root@anoly23.anoly23.ng.com>
2023-09-22 05:10:47 +0100

    Adds assembly code to file 101, that prints Hello, Holberton, followed by a new line

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,1 +1,17 @@
-i
+section .data
+	printstring db "Hello, Holberton", 10, 0
+
+section .text
+	global main
+	extern printf
+
+main:
+	push rbp
+	mov rdi, printstring
+	call printf
+	add rsp, 8
+	mov rax, 60
+	xor rdi, rdi
+	syscall
+
+section .note.GNU-stack
--- 0x12-singly_linked_lists/testing2.c
+++ 0x12-singly_linked_lists/testing2.c
@@ -1,9 +1,7 @@
-#include "lists.h"
 #include <stdio.h>
 
 int main(void)
 {
-	printf("Youat! and yet, you must allow,\nI bore my house upon my\
-	back!\n");
+	printf("Hello, Holberton");
 	return (0);
 }
