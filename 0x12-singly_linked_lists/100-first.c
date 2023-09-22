fc5b5042c28abc367e5f3f5c9889cf6ff037db23
root <root@anoly23.anoly23.ng.com>
2023-09-22 03:59:29 +0100

    Resolves Betty errors

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,8 +1,17 @@
 #include "lists.h"
 #include <stdio.h>
 
-__attribute__((constructor)) void print_before_main()
+__attribute__((constructor)) void print_before_main(void);
+
+/**
+ * print_before_main - prints a statement before executing the main function
+ * Definition - prints a statement
+ * Return: void
+*/
+
+void print_before_main(void)
+
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my\
-	back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
