bd9107248521045e56c1c8df4804c8f3ec5f5f93
root <root@anoly23.anoly23.ng.com>
2023-09-26 13:59:24 +0100

    Adds file 5-free_listint2.c to repo

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * free_listint2 - frees a linked list with integer values and sets
+ * head to NULL
+ * @head: pointer to pointer to the first node in linked list to be freed
+ * Definition - frees linked list and sets head to NULL
+ * Return: void
+*/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *iter;
+	listint_t *store;
+
+	iter = *head;
+
+	while (iter != NULL)
+	{
+		store = iter->next;
+		free(iter);
+		iter = store;
+	}
+	*head = NULL;
+}
