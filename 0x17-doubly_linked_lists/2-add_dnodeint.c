5f8e1c78d8dd201f5b2b91e036badea031ef44bf
Ano_ly <amarachiuvere@student.oauife.edu.ng>
2023-11-10 06:00:09 +0100

    Adds file 4

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,17 +11,15 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *iter;
 	dlistint_t *new;
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
 
-	iter = *head;
-	*head = new;
 	new->n = n;
-	new->next = iter;
+	new->next = *head;
 	new->prev = NULL;
+	*head = new;
 	return (new);
 }
