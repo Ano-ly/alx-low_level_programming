b650797b54880f3fb37cafa7e7ad62d11d7ff943
Ano_ly <amarachiuvere@student.oauife.edu.ng>
2023-11-11 04:19:00 +0100

    Adds file 6

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -6,8 +6,22 @@
  * @head: pointer to the first node of the list
  * Description - sums the integer values of all nodes of a linked list
  * Return: sum of values
+*/
 
 int sum_dlistint(dlistint_t *head)
 {
+	int sum;
+	dlistint_t *iter;
 
+	if (head == NULL)
+		return (0);
+	sum = 0;
+	iter = head;
+
+	while (iter != NULL)
+	{
+		sum += iter->n;
+		iter = iter->next;
+	}
+	return (sum);
 }
