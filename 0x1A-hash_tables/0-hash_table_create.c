e1b72a9dfc0a2253a0ba3f771c3670c9d992b902
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2023-12-02 03:51:32 +0100

    Edit file 0 once more

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,6 +12,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
+	unsigned long int i;
 
 	if (size > LONG_MAX)
 		return (NULL);
@@ -21,7 +22,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	new_table->size = size;
-	new_table->array = NULL;
+	new_table->array = calloc(size, sizeof(hash_node_t));
+
+	for (i = 0; i < size; i++)
+		new_table->array[i] = NULL;
+
 	return (new_table);
 
 }
