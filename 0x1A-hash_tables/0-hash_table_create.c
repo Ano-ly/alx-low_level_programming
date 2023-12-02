0a7316b9338c7aa33f378e97da1a3cf92d25eb10
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2023-12-02 04:01:54 +0100

    Edit file 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,12 +17,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (size > LONG_MAX)
 		return (NULL);
 
-	new_table = malloc(sizeof(hash_table_t));
+	new_table = (hash_table_t *)malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
 		return (NULL);
 
 	new_table->size = size;
-	new_table->array = calloc(size, sizeof(hash_node_t));
+	new_table->array = (hash_node_t **)calloc(size, sizeof(hash_node_t));
 
 	for (i = 0; i < size; i++)
 		new_table->array[i] = NULL;
