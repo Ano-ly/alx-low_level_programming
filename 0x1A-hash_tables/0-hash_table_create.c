e34ff1a6a2e0fd0d5d4fde19694ecb8b1db822ff
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2023-12-02 03:34:45 +0100

    Adds file 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a new hash table
+ * @size: size of new hash table to be created
+ * Definition - creates a new hash table
+ * Return: pointer to new hash table
+*/
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_table;
+
+	if (size <= 0)
+		return (NULL);
+	new_table = malloc(sizeof(hash_table_t));
+	if (new_table == NULL)
+		return (NULL);
+
+	new_table->size = size;
+	new_table->array = NULL;
+	return (new_table);
+
+}
