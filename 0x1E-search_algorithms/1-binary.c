df9d15f4ea83ec76c4f0ccb2068d7edf20fc2a6a
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-03-12 18:17:58 +0100

    Complete task 1

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for a value in an array
+ * using a binary search algorithm
+ * @array: array to be searched in
+ * @size: size of array
+ * @value: value to be searched for
+ * Description - searches an array for a value
+ * Return: index of the value, -1 if value not found
+*/
+
+int binary_search(int *array, size_t size, int value)
+{
+	int l_o;
+	int r_o;
+	int m_o;
+	int val;
+
+	if (array == NULL)
+		return (-1);
+	l_o = 0;
+	r_o = size - 1;
+	m_o = (int)((l_o + r_o) / 2);
+
+	val = rec(l_o, r_o, m_o, array, value);
+	return (val);
+}
+
+/**
+ * rec - performs recursive binary search
+ * @l_o: lower bound index
+ * @r_o: upper bound index
+ * @array: array to be searched in
+ * @value: value to be searched for
+ * @m_o: midpoint index
+ * Description - searches for a value using recursive binary
+ * search algorithm
+ * Return: index of value, -1 if not found
+*/
+
+int rec(int l_o, int r_o, int m_o, int *array, int value)
+{
+	print_array(l_o, r_o, array);
+	m_o = (int)((l_o + r_o) / 2);
+	if (array[m_o] == value)
+	{
+		return (m_o);
+	}
+	if (l_o >= r_o)
+		return (-1);
+	if (array[m_o] < value)
+		return (rec(m_o + 1, r_o, m_o, array, value));
+	if (array[m_o] > value)
+		return (rec(l_o, m_o - 1, m_o, array, value));
+	return (-1);
+}
+
+/**
+ * print_array - prints an array
+ * @l_o: lower bound index
+ * @r_o: upper bound index
+ * @array: array to be printed
+ * Description - prints an array
+ * Return: void
+*/
+
+void print_array(int l_o, int r_o, int *array)
+{
+	int i;
+
+	i = 0;
+
+	printf("Searching in array: ");
+	for (i = l_o; i < r_o + 1; i++)
+	{
+		printf("%d", array[i]);
+		if (i != r_o)
+			printf(", ");
+	}
+	printf("\n");
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,5 +3,7 @@
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int rec(int l_o, int r_o, int m_o, int *array, int value);
+void print_array(int l_o, int r_o, int *array);
 
 #endif /*SEARCH_ALGOS_H*/
