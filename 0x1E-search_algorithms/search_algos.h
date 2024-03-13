a6c5c2c19cb0001c4d917d77a3fe187dad8d2a2a
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-03-13 11:14:59 +0100

    Complete task 104

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,5 +8,6 @@ int binary_search(int *array, size_t size, int value);
 int rec(int l_o, int r_o, int m_o, int *array, int value);
 void print_array(int l_o, int r_o, int *array);
 int jump_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
 
 #endif /*SEARCH_ALGOS_H*/
